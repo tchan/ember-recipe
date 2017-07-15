214d0caac44116ce85d2e5ef4c546c587e97ddf9
Travis Chan <travismchan@gmail.com>
2017-07-15 16:59:35 +1000

    refactored most of the hasCode

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -105,6 +105,34 @@ function replaceFractionSymbol(recipeElement) {
   return recipeElement;
 }
 
+function convertValues(recipeElement, hasFunction, ingredientMultiplier, outputStr) {
+  let index = hasFunction(recipeElement);
+  // account for recipes like "1 1/2 cups sugar"
+  if (index >1 && !isNaN(recipeElement[0])) {
+    //combine the two into a single element
+    let combinedElement = recipeElement[0] + " " + recipeElement[1];
+    // remove double blank space due to bad code
+    // combinedElement = combinedElement.replace(/  +/g, ' '); linter complains about this one
+    combinedElement = combinedElement.replace(/ {2}/g, ' ');
+    recipeElement[1] = combinedElement;
+    recipeElement.shift();
+    index = hasFunction(recipeElement);
+  }
+
+  let valueIndex = index - 1;
+  if (recipeElement[valueIndex][0] == " ") {
+    recipeElement[valueIndex] = recipeElement[valueIndex].trim();
+  }
+  // accounts for  something lines starting with 1¾ -> ["1", "3/4"]
+  if (recipeElement[valueIndex].split(" ").length >1 ) {
+    recipeElement[valueIndex] = recipeElement[valueIndex].split(" ").join("+");
+  }
+
+  recipeElement[valueIndex] = (eval(recipeElement[valueIndex])*ingredientMultiplier).toFixed(0);
+  recipeElement[index] = outputStr;
+  return recipeElement;
+}
+
 export default Ember.Controller.extend({
   title: null,
   convertedRecipe: "Click convert!",
@@ -132,7 +160,7 @@ export default Ember.Controller.extend({
       //first replace fraction symbols
 
       //loop through each element and convert if needed
-      console.log('recipeArr', recipeArr);
+      // console.log('recipeArr', recipeArr);
       for (var i=0; i<recipeArr.length; i++) {
         // each line as a new array
         let recipeElement = recipeArr[i].split(" ");
@@ -145,9 +173,7 @@ export default Ember.Controller.extend({
 
         // Convert ounce value to grams if exists
         if (checkIfOunces(recipeElement)) {
-
           let ounce = checkIfOunces(recipeElement);
-          // console.log(ounce);
           let ounceValue = ounce - 1;
           recipeElement[ounceValue] = (gramsPerOunce*eval(recipeElement[ounceValue])).toFixed(0);
           recipeElement[ounce] = "grams";
@@ -157,45 +183,13 @@ export default Ember.Controller.extend({
 
         if (hasButter(recipeElement)) {
           // Convert cups to grams
-          // console.log('has butter');
           if (hasCup(recipeElement)) {
-            let butterCup = hasCup(recipeElement);
-            let butterCupValueIndex = butterCup - 1;
-            let butterCupValue = recipeElement[butterCupValueIndex];
-
-            if (butterCupValue[0] == " ") {
-              butterCupValue = butterCupValue.trim();
-            }
-            // console.log('buttercup value:', butterCupValue);
-            // console.log('buttercup valueArr:', butterCupValue.split(" "));
-
-            // accounts for  something lines starting with 1¾ -> ["1", "3/4"]
-            if (butterCupValue.split(" ").length > 1) {
-              butterCupValue = butterCupValue.split(" ").join("+");
-              // console.log('eval new ', eval(butterCupValue));
-            }
-            // console.log('butterCupPerGram', butterCupPerGram);
-            // console.log('buttercup value:', (eval(butterCupValue)*butterCupPerGram).toFixed(0));
-            recipeElement[butterCupValueIndex] = (eval(butterCupValue)*butterCupPerGram).toFixed(0);
-            recipeElement[butterCup] = "grams";
+            recipeElement = convertValues(recipeElement, hasCup, butterCupPerGram, "grams");
             recipeArr[i] =  recipeElement.join(" ");
           }
 
           if (hasTablespoon(recipeElement)) {
-            let butterTablespoon = hasTablespoon(recipeElement);
-            let butterTablespoonValueIndex = butterTablespoon - 1;
-            let butterTablespoonValue = recipeElement[butterTablespoonValueIndex];
-
-            if (butterTablespoonValue[0] == " ") {
-              butterTablespoonValue = butterTablespoonValue.trim();
-            }
-
-            if (butterTablespoonValue.split(" ").length > 1) {
-              butterTablespoonValue = butterTablespoonValue.split(" ").join("+");
-            }
-
-            recipeElement[butterTablespoonValueIndex] = (eval(butterTablespoonValue)*butterTbspPerGram).toFixed(0);
-            recipeElement[butterTablespoon] = "grams";
+            recipeElement = convertValues(recipeElement, hasTablespoon, butterTbspPerGram, "grams");
             recipeArr[i] =  recipeElement.join(" ");
           }
 
@@ -203,87 +197,21 @@ export default Ember.Controller.extend({
 
         if (hasSugar(recipeElement)) {
           if (hasCup(recipeElement)) {
-            // console.log('recipe element:', recipeElement);
-            let cupIndex = hasCup(recipeElement);
-            // account for recipes like "1 1/2 cups sugar"
-            if (cupIndex >1 && !isNaN(recipeElement[0])) {
-              //combine the two into a single element
-              let combinedElement = recipeElement[0] + " " + recipeElement[1];
-              // remove double blank space due to bad code
-              // combinedElement = combinedElement.replace(/  +/g, ' '); linter complains about this one
-              combinedElement = combinedElement.replace(/ {2}/g, ' ');
-              recipeElement[1] = combinedElement;
-              recipeElement.shift();
-
-              cupIndex = hasCup(recipeElement);
-            }
-
-            // console.log('cupindex', cupIndex);
-            let cupValueIndex = cupIndex - 1;
-
-            // accounts for something like ¾ cup rather than 1¾
-            if (recipeElement[cupValueIndex][0] == " ") {
-              recipeElement[cupValueIndex] = recipeElement[cupValueIndex].trim();
-            }
-
-            if (recipeElement[cupValueIndex].split(" ").length >1 ) {
-              recipeElement[cupValueIndex] = recipeElement[cupValueIndex].split(" ").join("+");
-            }
-            recipeElement[cupValueIndex] = (eval(recipeElement[cupValueIndex])*whiteSugarCupPerGram).toFixed(0);
-            recipeElement[cupIndex] = "grams";
+            recipeElement = convertValues(recipeElement, hasCup, whiteSugarCupPerGram, "grams");
             recipeArr[i] =  recipeElement.join(" ");
           }
         }
 
         if (hasFlour(recipeElement)) {
           if (hasCup(recipeElement)) {
-            let cupIndex = hasCup(recipeElement);
-            if (cupIndex >1 && !isNaN(recipeElement[0])) {
-
-              let combinedElement = recipeElement[0] + " " + recipeElement[1];
-              combinedElement = combinedElement.replace(/ {2}/g, ' ');
-              recipeElement[1] = combinedElement;
-              recipeElement.shift();
-              cupIndex = hasCup(recipeElement);
-            }
-
-            let cupValueIndex = cupIndex - 1;
-            if (recipeElement[cupValueIndex][0] == " ") {
-              recipeElement[cupValueIndex] = recipeElement[cupValueIndex].trim();
-            }
-
-            if (recipeElement[cupValueIndex].split(" ").length >1 ) {
-              recipeElement[cupValueIndex] = recipeElement[cupValueIndex].split(" ").join("+");
-            }
-
-            recipeElement[cupValueIndex] = (eval(recipeElement[cupValueIndex])*flourCupPerGram).toFixed(0);
-            recipeElement[cupIndex] = "grams";
+            recipeElement = convertValues(recipeElement, hasCup, flourCupPerGram, "grams");
             recipeArr[i] =  recipeElement.join(" ");
           }
         }
 
         if (hasCocoaPowder(recipeElement)) {
           if (hasCup(recipeElement)) {
-            let cupIndex = hasCup(recipeElement);
-            if (cupIndex >1 && !isNaN(recipeElement[0])) {
-
-              let combinedElement = recipeElement[0] + " " + recipeElement[1];
-              combinedElement = combinedElement.replace(/ {2}/g, ' ');
-              recipeElement[1] = combinedElement;
-              recipeElement.shift();
-              cupIndex = hasCup(recipeElement);
-            }
-            let cupValueIndex = cupIndex - 1;
-            if (recipeElement[cupValueIndex][0] == " ") {
-              recipeElement[cupValueIndex] = recipeElement[cupValueIndex].trim();
-            }
-
-            if (recipeElement[cupValueIndex].split(" ").length >1 ) {
-              recipeElement[cupValueIndex] = recipeElement[cupValueIndex].split(" ").join("+");
-            }
-
-            recipeElement[cupValueIndex] = (eval(recipeElement[cupValueIndex])*cocoaPowderPerGram).toFixed(0);
-            recipeElement[cupIndex] = "grams";
+            recipeElement = convertValues(recipeElement, hasCup, cocoaPowderPerGram, "grams");
             recipeArr[i] =  recipeElement.join(" ");
           }
 
@@ -291,54 +219,14 @@ export default Ember.Controller.extend({
 
         if (hasLiquid(recipeElement)) {
           if (hasCup(recipeElement)) {
-            let cupIndex = hasCup(recipeElement);
-            if (cupIndex >1 && !isNaN(recipeElement[0])) {
-
-              let combinedElement = recipeElement[0] + " " + recipeElement[1];
-              combinedElement = combinedElement.replace(/ {2}/g, ' ');
-              recipeElement[1] = combinedElement;
-              recipeElement.shift();
-              cupIndex = hasCup(recipeElement);
-            }
-
-            let cupValueIndex = cupIndex - 1;
-
-            if (recipeElement[cupValueIndex][0] == " ") {
-              recipeElement[cupValueIndex] = recipeElement[cupValueIndex].trim();
-            }
-
-            if (recipeElement[cupValueIndex].split(" ").length >1 ) {
-              recipeElement[cupValueIndex] = recipeElement[cupValueIndex].split(" ").join("+");
-            }
-
-            recipeElement[cupValueIndex] = (eval(recipeElement[cupValueIndex])*mlsPerCup).toFixed(0);
-            recipeElement[cupIndex] = "ml";
+            recipeElement = convertValues(recipeElement, hasCup, mlsPerCup, "ml");
             recipeArr[i] =  recipeElement.join(" ");
           }
         }
 
         if (hasVanillaExtract(recipeElement)) {
           if (hasTablespoon(recipeElement)) {
-            let TablespoonIndex = hasTablespoon(recipeElement);
-            if (TablespoonIndex >1 && !isNaN(recipeElement[0])) {
-              let combinedElement = recipeElement[0] + " " + recipeElement[1];
-              combinedElement = combinedElement.replace(/ {2}/g, ' ');
-              recipeElement[1] = combinedElement;
-              recipeElement.shift();
-              TablespoonIndex = hasTablespoon(recipeElement);
-            }
-
-            let TablespoonValueIndex = TablespoonIndex - 1;
-            if (recipeElement[TablespoonValueIndex][0] == " ") {
-              recipeElement[TablespoonValueIndex] = recipeElement[TablespoonValueIndex].trim();
-            }
-
-            if (recipeElement[TablespoonValueIndex].split(" ").length >1 ) {
-              recipeElement[TablespoonValueIndex] = recipeElement[TablespoonValueIndex].split(" ").join("+");
-            }
-
-            recipeElement[TablespoonValueIndex] = (eval(recipeElement[TablespoonValueIndex])*vanillaExtractMlPerTbsp).toFixed(0);
-            recipeElement[TablespoonIndex] = "ml";
+            recipeElement = convertValues(recipeElement, hasTablespoon, vanillaExtractMlPerTbsp, "ml");
             recipeArr[i] =  recipeElement.join(" ");
           }
         }
